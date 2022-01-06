f812b4c66263e9fec4d14adbfd9bca51d7a46d7e
Sara <lopezlovonsara@gmail.com>
2022-01-06 18:58:48 +0100

    add conditional sort

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -5,16 +5,64 @@ const products = {
     getAllProducts : async (req, res) =>{
     
         try{
-            const { page = 1, perPage = 10, sort ={name: 1, price:1}  } = req.query;
-            const options ={
-                page: parseInt(page,10),
-                limit: parseInt(perPage,10),
-                sort: sort,
-                populate: {path:'id_provider'}
-            }
 
-            const data = await Products.paginate({}, options)
-            res.status(200).json(data)
+            await Products.updateMany(
+                { 'price' : { $type: 2 } },
+                [{ $set: { 'price': { $toDouble: "$price" } } }]
+            )
+
+            if(req.query.name){
+                
+                //http://localhost:3000/?perPage=5&page=2&name=1
+                const { page = 1, perPage = 10, name} = req.query;
+                const options ={
+                    page: parseInt(page,10),
+                    limit: parseInt(perPage,10),
+                    sort: {name},
+                    populate: {path:'id_provider'}
+                }
+                const data = await Products.paginate({}, options)
+                res.status(200).json(data)
+
+            }else if(req.query.rating){
+
+                //http://localhost:3000/?perPage=5&page=2&rating=1
+                const { page = 1, perPage = 10, rating} = req.query;
+                const options ={
+                    page: parseInt(page,10),
+                    limit: parseInt(perPage,10),
+                    sort: {rating},
+                    populate: {path:'id_provider'}
+                }
+                const data = await Products.paginate({}, options)
+                res.status(200).json(data)
+
+            }else if(req.query.price){
+
+                //http://localhost:3000/?perPage=5&page=2&price=1
+                const { page = 1, perPage = 10, price} = req.query;
+                const options ={
+                    page: parseInt(page,10),
+                    limit: parseInt(perPage,10),
+                    sort: {price},
+                    populate: {path:'id_provider'}
+                }
+                const data = await Products.paginate({}, options)
+                res.status(200).json(data)
+
+            }else{
+
+                //http://localhost:3000/?perPage=5&page=2
+                const { page = 1, perPage = 10} = req.query;
+                const options ={
+                    page: parseInt(page,10),
+                    limit: parseInt(perPage,10),                    
+                    populate: {path:'id_provider'}
+                }
+                const data = await Products.paginate({}, options)
+                res.status(200).json(data)
+
+            }
 
         } catch (err){
             res.status(400).json({'error':err})
@@ -29,18 +77,60 @@ const products = {
                 return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
             };
 
-            if (req.query.nameProduct) {
+            if (req.query.nameProduct && req.query.name) {
 
-                const { page = 1, perPage = 10, sort ={name: 1, price:1} } = req.query;
+                //http://localhost:3000/products/search?nameProduct=silla&name=1
+                const { page = 1, perPage = 10, name} = req.query;
                 const options ={
                 page: parseInt(page,10),
                 limit: parseInt(perPage,10),
-                sort: sort
+                sort: {name}
                 }
 
                 const regex = new RegExp(escapeRegex(req.query.nameProduct), 'gi');
                 data = await Products.paginate({$or: [{name:regex}, {provider: regex}]}, options)
                 res.status(200).json(data)
+
+            }else if(req.query.nameProduct && req.query.rating) {
+
+                //http://localhost:3000/products/search?nameProduct=silla&rating=1
+                const { page = 1, perPage = 10, rating} = req.query;
+                const options ={
+                page: parseInt(page,10),
+                limit: parseInt(perPage,10),
+                sort: {rating}
+                }
+
+                const regex = new RegExp(escapeRegex(req.query.nameProduct), 'gi');
+                data = await Products.paginate({$or: [{name:regex}, {provider: regex}]}, options)
+                res.status(200).json(data)
+
+            }else if(req.query.nameProduct && req.query.price) {
+
+                //http://localhost:3000/products/search?nameProduct=silla&price=1
+                const { page = 1, perPage = 10, price} = req.query;
+                const options ={
+                page: parseInt(page,10),
+                limit: parseInt(perPage,10),
+                sort: {price}
+                }
+
+                const regex = new RegExp(escapeRegex(req.query.nameProduct), 'gi');
+                data = await Products.paginate({$or: [{name:regex}, {provider: regex}]}, options)
+                res.status(200).json(data)
+
+            }else{
+
+                const { page = 1, perPage = 10} = req.query;
+                const options ={
+                page: parseInt(page,10),
+                limit: parseInt(perPage,10)
+                }
+
+                const regex = new RegExp(escapeRegex(req.query.nameProduct), 'gi');
+                data = await Products.paginate({$or: [{name:regex}, {provider: regex}]}, options)
+                res.status(200).json(data)
+
             }
 
         }catch(err){
